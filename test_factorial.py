4abaffba1ae8498e7d70f725d2222cc834cf91e5
dcheng <102439573+dcheng2022@users.noreply.github.com>
2024-06-26 13:51:11 -0500

    add files

---
+++ test_factorial.py
@@ -0,0 +1,13 @@
+from factorial import factorial
+
+def test_zero():
+    assert factorial(0) == 0
+
+def test_one():
+    assert factorial(1) == 1
+
+def test_two():
+    assert factorial(2) == 2
+
+def test_five():
+    assert factorial(5) == 120
